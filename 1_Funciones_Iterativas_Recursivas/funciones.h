46725037c7f10d34c05d4b5f7644418dfff4dfef
EmanuelVegaGlz <111370943+EmanuelVegaGlz@users.noreply.github.com>
2023-08-17 18:57:49 -0600

    Add files via upload

--- 1_Funciones_Iterativas_Recursivas/funciones.h
+++ 1_Funciones_Iterativas_Recursivas/funciones.h
@@ -1,1 +1,23 @@
+class Funciones{
+    public:
+    int sumaIterativa(int num){
+        int acum = 0;
+        for(int i = 1; i <= num; i++){
+            acum += i;
+        }
+        return acum;
+    }
 
+    int sumaRecursiva(int num){
+        if(num == 0)
+	        return 0;
+
+        else 
+            return num + sumaRecursiva(num - 1);
+    }
+
+    int sumaDirecta(int num){
+        return (num*(num+1))/2;
+    }
+
+};
